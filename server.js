f771bcca538494dd81cbdd9bc473d1b3732bb7bb
Christian Killelea <christiankillelea@Christians-MacBook-Air.local>
2023-06-13 23:11:23 -0500

    commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('/controllers');
-const helpers = require('/utils/helpers');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-const sequelize = require('/config/connection');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -19,8 +19,8 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -35,5 +35,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Server is running on port', PORT));
 });
